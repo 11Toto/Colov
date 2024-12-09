534270949524b37f1ce4505dd88bdbc9c4cb943b
Antoine <>
2024-12-09 10:57:34 +0100

    Script Mort
    quand on meurt on reçoit un message de la couleur qui nous tue puis on est bloqué sur place en speed 0 et le script PlayerDeath a été enlevé pour etre fusionner avec CharcterMovement

--- Colov/Assets/script/CharacterMovement.cs
+++ Colov/Assets/script/CharacterMovement.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerDeath : MonoBehaviour
+public class CharacterMovement : MonoBehaviour
 {
-    
-
-    // Update is called once per frame
+    public int speed = 3;
     void Update()
     {
-        
+        Vector3 scrolling = Vector3.zero;
+        scrolling.x = speed;
+
+        Vector3 movement = scrolling * Time.deltaTime;
+        transform.Translate(movement);
     }
 
     void OnTriggerEnter(Collider other)
@@ -17,22 +19,28 @@ public class PlayerDeath : MonoBehaviour
         if (transform.gameObject.tag != "Red" && other.CompareTag("RedObstacle"))
         {
              Debug.Log("Tu es Mort par Red");
+             speed = 0;
         }
         if (transform.gameObject.tag != "Blue" && other.CompareTag("BlueObstacle"))
         {
              Debug.Log("Tu es Mort par Blue");
+             speed = 0;
         }
         if (transform.gameObject.tag != "Green" && other.CompareTag("GreenObstacle"))
         {
              Debug.Log("Tu es Mort par Green");
+             speed = 0;
         }
         if (transform.gameObject.tag != "Yellow" && other.CompareTag("YellowObstacle"))
         {
              Debug.Log("Tu es Mort par Yellow");
+             speed = 0;
         }
         if (transform.gameObject.tag != "Purple" && other.CompareTag("PurpleObstacle"))
         {
              Debug.Log("Tu es Mort par Purple");
+             speed = 0;
         }
     }
+
 }
