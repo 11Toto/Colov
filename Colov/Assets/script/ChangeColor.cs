5df3558d5d6c122f3710f89810c5fcc05da15aef
Antoine <>
2024-12-09 11:50:33 +0100

    Changement de couleur unique par niveaux + tag niveau  1 - 2 - 3
    lorsque qu'on est sur le niveau 1 on a 3 couleurs (Rouge, Bleu, Vert), niveau 2 on a 4 couleurs (Rouge, Bleu, Vert, Jaune), niveau 3 on a 5 couleurs (Rouge, Bleu, Vert, Jaune, Violet)

--- Colov/Assets/script/ChangeColor.cs
+++ Colov/Assets/script/ChangeColor.cs
@@ -19,44 +19,104 @@ public class ChangeColor : MonoBehaviour
     
     void Update()
     {
-       ChangeTagColor();
+       
        ChangeSpriteColor();
     }
 
     void ChangeTagColor()
     {
-        if (transform.gameObject.tag == "Red")
+        
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Niveau1")
+        {
+            Debug.Log("Niveau1");
+            if (transform.gameObject.tag == "Red")
                     if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
                     {
                         transform.gameObject.tag = "Blue";
                         timestamp = Time.time + timeBetweenChange;
                     }
-
-                if (transform.gameObject.tag == "Blue")
-                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space))
+            if (transform.gameObject.tag == "Blue")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
                     {
                         transform.gameObject.tag = "Green";
                         timestamp = Time.time + timeBetweenChange;
                     }
-                if (transform.gameObject.tag == "Green")
-                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space))
+            if (transform.gameObject.tag == "Green")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
+                    {
+                        transform.gameObject.tag = "Red";
+                        timestamp = Time.time + timeBetweenChange;
+                    }
+
+        }
+
+        else if (other.gameObject.tag == "Niveau2")
+        {
+            Debug.Log("Niveau2");
+            if (transform.gameObject.tag == "Red")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
+                    {
+                        transform.gameObject.tag = "Blue";
+                        timestamp = Time.time + timeBetweenChange;
+                    }
+            if (transform.gameObject.tag == "Blue")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
+                    {
+                        transform.gameObject.tag = "Green";
+                        timestamp = Time.time + timeBetweenChange;
+                    }
+            if (transform.gameObject.tag == "Green")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
                     {
                         transform.gameObject.tag = "Yellow";
                         timestamp = Time.time + timeBetweenChange;
                     }
-                if (transform.gameObject.tag == "Yellow")
-                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space))
+            if (transform.gameObject.tag == "Yellow")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
+                    {
+                        transform.gameObject.tag = "Red";
+                        timestamp = Time.time + timeBetweenChange;
+                    }
+        }
+
+        else if (other.gameObject.tag == "Niveau3")
+        {
+            Debug.Log("Niveau3");
+            if (transform.gameObject.tag == "Red")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
+                    {
+                        transform.gameObject.tag = "Blue";
+                        timestamp = Time.time + timeBetweenChange;
+                    }
+            if (transform.gameObject.tag == "Blue")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
+                    {
+                        transform.gameObject.tag = "Green";
+                        timestamp = Time.time + timeBetweenChange;
+                    }
+            if (transform.gameObject.tag == "Green")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
+                    {
+                        transform.gameObject.tag = "Yellow";
+                        timestamp = Time.time + timeBetweenChange;
+                    }
+            if (transform.gameObject.tag == "Yellow")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
                     {
                         transform.gameObject.tag = "Purple";
                         timestamp = Time.time + timeBetweenChange;
                     }
-                if (transform.gameObject.tag == "Purple")
-                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space))
+            if (transform.gameObject.tag == "Purple")
+                    if (Time.time >= timestamp && Input.GetKey(KeyCode.Space)) 
                     {
                         transform.gameObject.tag = "Red";
                         timestamp = Time.time + timeBetweenChange;
-                    }    
-
+                    }
+        }
     }
 
     void ChangeSpriteColor()
