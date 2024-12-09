f2b5191fecaa05aefb398d6cf186893c13642b6c
Linkdu <chaillouxmarius@gmail.com>
2024-12-09 11:10:59 +0100

    nouvelle scène Marius + script mouvement update + materials

--- Colov/Assets/Scripts/CharacterMovement.cs
+++ Colov/Assets/Scripts/CharacterMovement.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
     public int speed = 3;
-    public string badcolor = "Vert";
+    public string GoodColor;
+
+    private void Start()
+    {
+        GoodColor = gameObject.tag;
+    }
     void Update()
     {
         Vector3 scrolling = Vector3.zero;
@@ -17,8 +23,12 @@ public class CharacterMovement : MonoBehaviour
 
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log("touché");
-        if (other.tag == badcolor)
+        Debug.Log("touch");
+        if (other.tag == GoodColor)
+        {
+            Debug.Log("pass");
+        }
+        else
         {
             Debug.Log("stop");
             speed = 0;
