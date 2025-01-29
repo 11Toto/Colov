e8505224fbe5bea156c86d5c86ea47e540181502
Antoine <>
2025-01-30 00:00:46 +0100

    Changement script loadscene pour son + Logo jeu

--- Colov/Assets/script/LoadScene.cs
+++ Colov/Assets/script/LoadScene.cs
@@ -7,6 +7,7 @@ public class LoadScene : MonoBehaviour
 {
     public bool Anim = false;
     float timeAnim = 0.5f;
+    float timeButton = 0.7f;
 
 
     IEnumerator TimerAnim()
@@ -15,48 +16,90 @@ public class LoadScene : MonoBehaviour
         SceneManager.LoadScene("StoryBoard_Scene");
     }
 
-    public void LoadGameVF()
+    IEnumerator TimerButtonGameVF()
     {
+        yield return new WaitForSeconds(timeButton);
         Anim = true;
         StartCoroutine(TimerAnim());
+    }
+    IEnumerator TimerButtonGameVFRestart()
+    {
+        yield return new WaitForSeconds(timeButton);
+        SceneManager.LoadScene("Game_Scene_LVL1");
+        
+    }
+    IEnumerator TimerButtonMenuVF()
+    {
+        yield return new WaitForSeconds(timeButton);
+        SceneManager.LoadScene("Menu_Scene_VF");
+        
+    }
+    IEnumerator TimerButtonOptionVF()
+    {
+        yield return new WaitForSeconds(timeButton);
+        SceneManager.LoadScene("Option_Scene_VF");
+        
+    }
+    IEnumerator TimerButtonGameLVL2()
+    {
+        yield return new WaitForSeconds(timeButton);
+        SceneManager.LoadScene("Game_Scene_LVL2");
+        
+    }
+    IEnumerator TimerButtonGameLVL3()
+    {
+        yield return new WaitForSeconds(timeButton);
+        SceneManager.LoadScene("Game_Scene_LVL3");
+        
+    }
+    IEnumerator TimerQuitGame()
+    {
+        yield return new WaitForSeconds(timeButton);
+        Application.Quit();
+        Debug.Log("Au revoir");
+        Anim = true;
+        
+    }
+
+    
+
+    public void LoadGameVF()
+    {
+        StartCoroutine(TimerButtonGameVF());
     }
 
     public void LoadGameVFRestart()
     {
-        SceneManager.LoadScene("Game_Scene_LVL1");
+        StartCoroutine(TimerButtonGameVFRestart());
     }
 
 
     public void LoadMenuVF()
     {
-        SceneManager.LoadScene("Menu_Scene_VF");
+        StartCoroutine(TimerButtonMenuVF());
     }
 
 
     public void LoadOptionVF()
     {
-        SceneManager.LoadScene("Option_Scene_VF");
+       StartCoroutine(TimerButtonOptionVF());
     }
 
 
     public void LoadGameLVL2()
     {
-        SceneManager.LoadScene("Game_Scene_LVL2");
+       StartCoroutine(TimerButtonGameLVL2());
     }
 
     public void LoadGameLVL3()
     {
-        SceneManager.LoadScene("Game_Scene_LVL3");
+       StartCoroutine(TimerButtonGameLVL3());
     }
 
 
 
     public void QuitGame()
     {
-        Application.Quit();
-        Debug.Log("Au revoir");
-        Anim = true;
+        StartCoroutine(TimerQuitGame());
     }
-
-    
 }
